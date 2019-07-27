48d8145b7a2e686c0eebf3d22b139a35a9cb4ced
kakakakakku <y.yoshida22@gmail.com>
2019-07-27 19:11:18 +0900

    Lesson 3

---
+++ hello-lesson3.lua
@@ -0,0 +1,23 @@
+-- 変数とデータ型
+
+message = "hello world"
+print(message)
+
+--[[
+数値
+文字列
+真偽値 true false
+nil
+テーブル
+関数
+
+* 型は動的
+* 多重代入
+]]
+
+x, y = 10, 15
+print(x)
+print(y)
+x, y = y, x
+print(x)
+print(y)

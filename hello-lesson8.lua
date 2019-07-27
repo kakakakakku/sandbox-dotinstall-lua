b3d41ab0dbe95e81fdb484d0f8ecfa7f3a7a7bb5
kakakakakku <y.yoshida22@gmail.com>
2019-07-27 19:27:29 +0900

    Lesson 8

---
+++ hello-lesson8.lua
@@ -0,0 +1,26 @@
+-- 条件分岐 if
+
+--[[
+
+関係演算子
+
+< > <= >=
+==
+~=
+
+論理演算子
+
+and or not
+
+真偽の違い
+
+偽 : false nil
+真 : それ以外 0 ''
+
+]]
+
+score = 75
+
+if score > 60 and score < 80 then
+    print("ok!")
+end

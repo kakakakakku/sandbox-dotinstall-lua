d89dcc97c555e549020f3a00605ef8a40b9f5938
kakakakakku <y.yoshida22@gmail.com>
2019-07-27 19:45:32 +0900

    Lesson 13

---
+++ hello-lesson13.lua
@@ -0,0 +1,15 @@
+-- 数値
+
+--[[
+math.max(2, 222, 14) -- 222
+math.min(2, 222, 14) -- 2
+math.ceil(2.3) -- 3
+math.floor(2.3) -- 2
+
+math.random() -- 0以上、1未満
+math.random(n) -- 1からnまで整数値
+math.random(20, 100)
+]]
+
+x = math.random(20, 100)
+print(x)

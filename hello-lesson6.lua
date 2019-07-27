f95662ba7baf3a932f7af043bbb1df95f6721580
kakakakakku <y.yoshida22@gmail.com>
2019-07-27 19:20:56 +0900

    Lesson 6

---
+++ hello-lesson6.lua
@@ -0,0 +1,16 @@
+-- テーブル（配列/連想配列）
+
+a = {23, 234, "hello"}
+
+print(a[2])
+
+a[2] = "hey"
+
+print(a[2])
+
+print(#a)
+
+user = {name = "kakakakakku", score = 120}
+
+print(user["name"])
+print(user.score)

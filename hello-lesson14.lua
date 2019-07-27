e243ed5fe74cb06d47ca882c218ce9d28c50dd4a
kakakakakku <y.yoshida22@gmail.com>
2019-07-27 19:49:13 +0900

    Lesson 14

---
+++ hello-lesson14.lua
@@ -0,0 +1,14 @@
+-- 文字列
+
+--[[
+s = string.len("dotinstall")
+s = #"dotinstall"
+s = string.sub("dotinstall", 3, 5)
+s = string.find("dotinstall", "s")
+s = string.gsub("dotinstall", "dot", ".")
+s = string.upper("dotinstall")
+]]
+
+s = string.reverse("dotinstall")
+
+print(s)
